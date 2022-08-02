cbdc9cdc18342d05bec897463f1b10fc9d65dac0
amrw-js <81330789+amrw-js@users.noreply.github.com>
2022-08-02 17:43:03 +0000

    Deploying to gh-pages from @ scaleflex/next-cloudimage-responsive@4086acd18ecf6d9bb2a8d261d7086929c9c6d53e 🚀

--- _next/static/i75YGXQSt_FIXW2Yon2rC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/422-4bf3c4da72b98155.js","static/css/43d54e30c3ea24b5.css","static/chunks/pages/index-aa8dde84b567a02d.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
