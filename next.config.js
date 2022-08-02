70debb1eafd779b062d449912d134260312bb99c
Amr Wagdy <wagdy@scaleflex.com>
2022-08-02 19:56:53 +0200

    Chore: Deploy demo

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   images: {
     loader: 'custom',
     path: 'https://scaleflex.cloudimg.io/',
   },
-  assetPrefix: './',
+  ...(isProd ? {
+    assetPrefix: '/next-cloudimage-responsive/',
+    basePath: '/next-cloudimage-responsive',
+  } : {}),
 };
 
 module.exports = nextConfig;
