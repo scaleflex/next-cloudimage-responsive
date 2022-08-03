acb906148f006234fc54df026e0887da6e92a268
Amr Wagdy <wagdy@scaleflex.com>
2022-08-03 02:33:27 +0200

    Updates

--- _next/static/Izz6uxno9_JGqdIUogGPG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/422-a1fe7aa76677ce6f.js","static/css/4d1e898b821be0b6.css","static/chunks/pages/index-abda3afd051f404b.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
