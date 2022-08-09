b2eb86bbd8cd6c6c0d5c55e73c9e0f2a273058ba
Amr Wagdy <wagdy@scaleflex.com>
2022-08-09 13:55:59 +0200

    Updates

--- _next/static/yvNI7I_28d4USciPU1vMw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/422-7f2ac36009f89dec.js","static/css/4d1e898b821be0b6.css","static/chunks/pages/index-d9804460e3d0ef73.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
