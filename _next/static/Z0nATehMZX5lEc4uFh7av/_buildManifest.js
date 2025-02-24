e64ab375f1728b384fbf00b9840d8026369f29e6
AhmeeedMostafa <ahmed.mostafa1198@gmail.com>
2025-02-24 14:17:11 +0000

    Deploying to gh-pages from @ scaleflex/next-cloudimage-responsive@029eb6407d67b0c8fec57fd87be02416045fb8a6 🚀

--- _next/static/Z0nATehMZX5lEc4uFh7av/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/422-9abf57a678eb9504.js","static/css/dc8cf15b462a392d.css","static/chunks/pages/index-6e2685a9e872b733.js"],"/_error":["static/chunks/pages/_error-670508f436d772db.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
